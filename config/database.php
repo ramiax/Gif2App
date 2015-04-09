a4634a68cbe3b0cfe3413c659ded6be86398f39d
Dominykas Vilimas <dominykas.vilimas@gmail.com>
2015-04-09 13:36:25 +0300

    Theme change 2

--- config/database.php
+++ config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => 'localhost',
+			'database'  => 'gif2appdb',
+			'username'  => 'gif2appdb',
+			'password'  => 'tjtfwGEBv62Rm2Pn',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
